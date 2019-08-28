0249c3ae8fa24ccdd3e2338d939764f25e56998d
Misya <neko@Misya.local>
2019-08-28 22:58:14 +0900

    move~ 系のリンクを貼るシェルを追加

--- startup.sh
+++ startup.sh
@@ -3,3 +3,8 @@
 
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/_vimrc ~/_vimrc
+
+#ターミナル起動時に自動実行されるshellスクリプト
+ln -sf ~/dotfiles/moveGif.sh  ~/moveGif.sh
+ln -sf ~/dotfiles/moveScreenshot.sh  ~/moveScreenshot.sh
+
