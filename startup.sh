b1e0d7aeb9dfbeae962a422ec0c4ff4f9c1672e9
しゃくしも　猫も <shakushimonekomo@shakushimonekomonoMacBook-Pro.local>
2019-10-27 13:51:08 +0900

    kalisiとvim-plugを自動dlできるように変更

--- startup.sh
+++ startup.sh
@@ -10,3 +10,15 @@ ln -sf ~/dotfiles/moveGif.sh  ~/.moveGif.sh
 ln -sf ~/dotfiles/moveScreenshot.sh  ~/.moveScreenshot.sh
 ln -sf ~/dotfiles/moveMd.sh  ~/.moveMd.sh
 
+#vimの設定ファイル用フォルダを作成
+mkdir ~/.vim
+#カラースキームの保存先フォルダを作成
+mkdir ~/.vim/colors
+
+#plug.vimのダウンロード&autoloadに配置
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#colorscheme kalisi をダウンロード & 配置
+git clone https://github.com/freeo/vim-kalisi ~/.vim/colors/kalisi
+mv ~/.vim/colors/kalisi/colors/kalisi.vim ~/.vim/colors/kalisi.vim
+rm -rf ~/.vim/colors/kalisi
